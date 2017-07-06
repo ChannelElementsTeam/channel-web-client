37f45bf0bc434d51de9dac39403da9207b73776c
Preet <preet@hivepoint.com>
2017-07-06 16:54:30 -0700

    join page

--- public/src/home/home-page.js
+++ public/src/home/home-page.js
@@ -8,9 +8,29 @@ class HomePage extends Polymer.Element {
   }
 
   onJoin() {
+    this._join();
+  }
+
+  _join(code) {
     Polymer.importHref(this.resolveUrl("../dialogs/join-channel-dlg.html"), () => {
-      this.$.dlgJoin.show();
+      this.$.dlgJoin.show(code);
     });
+  }
+
+  onActivate(route) {
+    const hash = route.segments[0];
+    if (hash) {
+      switch (hash) {
+        case "join": {
+          const code = route.segments[1];
+          if (code) {
+            this._join(code);
+          }
+        }
+        default:
+          break;
+      }
+    }
   }
 }
 
