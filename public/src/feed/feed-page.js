a5d316b462567d1dedbe905afad75ed0ac496ece
Preet <preet@hivepoint.com>
2017-07-12 12:33:06 -0700

    aded feed controls

--- public/src/feed/feed-page.js
+++ public/src/feed/feed-page.js
@@ -7,6 +7,23 @@ class FeedPage extends Polymer.Element {
   }
 
   onActivate() {
+    const barActions = [
+      {
+        icon: "chnls:add",
+        name: "New channel",
+        callback: () => { this.onCreate(); }
+      },
+      {
+        icon: "chnls:share",
+        name: "join a channel",
+        callback: () => { this.join(); }
+      }
+    ];
+    $app.setBarData({
+      title: "Channels",
+      actions: barActions
+    });
+
     this.refreshChannels();
   }
 
@@ -15,15 +32,34 @@ class FeedPage extends Polymer.Element {
   }
 
   refreshChannels() {
+    this.set("channels", []);
     $channels.listAllChannels($service.identityManager.signedAddress).then((list) => {
       this.set("channels", list);
       console.log("channels: ", list);
+    }).catch((err) => {
+      console.error("Failed to load list of channels: ", err);
     });
   }
 
   onItemClick(event) {
     const data = event.model.item;
     $router.goto(['channel', data.providerId, data.channelAddress]);
+  }
+
+  onCreate() {
+    Polymer.importHref(this.resolveUrl("../dialogs/create-channel-dlg.html"), () => {
+      this.$.dlgCreate.show();
+    });
+  }
+
+  join() {
+    this._join();
+  }
+
+  _join(code) {
+    Polymer.importHref(this.resolveUrl("../dialogs/join-channel-dlg.html"), () => {
+      this.$.dlgJoin.show(code);
+    });
   }
 }
 window.customElements.define(FeedPage.is, FeedPage);
