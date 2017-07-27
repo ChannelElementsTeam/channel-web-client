be87098fd206dc30b696dba93e8bcb0f2e5117ee
Preet <preet@hivepoint.com>
2017-07-27 14:21:52 -0700

    .

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ import { RestServer } from './interfaces/rest-server';
 import { UrlManager } from './url-manager';
 import { rootPageHandler } from './page-handlers/root-handler';
 
-const VERSION = 11;
+const VERSION = 12;
 
 class ChannelElementsWebClient {
   private app: express.Application;
